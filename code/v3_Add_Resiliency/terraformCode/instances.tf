4ca61578d9dd4945fe3ff0995fc42d743e758490
Nirav Soni <soninirav910@gmail.com>
2024-02-20 21:57:22 +0530

    Updates

--- code/v3_Add_Resiliency/terraformCode/instances.tf
+++ code/v3_Add_Resiliency/terraformCode/instances.tf
@@ -19,6 +19,22 @@ resource "aws_instance" "nginx1" {
 sudo amazon-linux-extras install -y nginx1
 sudo service nginx start
 sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+echo '<html><head><title>Taco Team Server 1</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+EOF
+}
+
+# Another instance that sits in subnet 2
+resource "aws_instance" "nginx2" {
+  ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet2.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  tags                   = local.common_tags
+  user_data              = <<EOF
+#! /bin/bash
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><head><title>Taco Team Server 2</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
 EOF
 }
--- code/v3_Add_Resiliency/terraformCode/network.tf
+++ code/v3_Add_Resiliency/terraformCode/network.tf
@@ -35,12 +35,23 @@ resource "aws_internet_gateway" "app" {
   tags   = local.common_tags
 }
 
-# Creating a subnet and associating with VPC (Using VPC Id)
+# Creating first subnet in a AZ1
 resource "aws_subnet" "public_subnet1" {
-  cidr_block              = var.vpc_public_subnet1_cidr_block
+  cidr_block              = var.vpc_public_subnets_cidr_block[0]
   vpc_id                  = aws_vpc.app.id
   map_public_ip_on_launch = var.map_public_ip_on_launch
   tags                    = local.common_tags
+  # Assigning Subnet-1 with first Availability zone retrieved from data (We get this information from AWS provider based on region we have configured)
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+# Creating another subnet in a AZ2
+resource "aws_subnet" "public_subnet2" {
+  cidr_block              = var.vpc_public_subnets_cidr_block[1]
+  vpc_id                  = aws_vpc.app.id
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  tags                    = local.common_tags
+  availability_zone = data.aws_availability_zones.available.names[1]
 }
 
 # Creating route table and associate with VPC
@@ -53,9 +64,15 @@ resource "aws_route_table" "app" {
   tags = local.common_tags
 }
 
-# Associating route table with Subnet
+# Associating route table with Subnet1
 resource "aws_route_table_association" "app_subnet1" {
   subnet_id      = aws_subnet.public_subnet1.id
+  route_table_id = aws_route_table.app.id
+}
+
+# Associating route table with Subnet2
+resource "aws_route_table_association" "app_subnet2" {
+  subnet_id      = aws_subnet.public_subnet2.id
   route_table_id = aws_route_table.app.id
 }
 
@@ -63,6 +80,30 @@ resource "aws_route_table_association" "app_subnet1" {
 # Creating Security group inside VPC
 resource "aws_security_group" "nginx_sg" {
   name   = "nginx_sg"
+  vpc_id = aws_vpc.app.id
+  tags   = local.common_tags
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Making this change since now, EC2 instance should receive traffic from addresses within VPC
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Provisioning Security group for Load balancer
+resource "aws_security_group" "alb_sg" {
+  name   = "nginx_alb_sg"
   vpc_id = aws_vpc.app.id
   tags   = local.common_tags
 

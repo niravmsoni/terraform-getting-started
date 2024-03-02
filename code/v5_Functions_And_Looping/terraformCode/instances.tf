99d03aa427a2fa996b8a52aed447ea43fbb7e504
Nirav Soni <soninirav910@gmail.com>
2024-03-02 14:38:53 +0530

    Move userdata to separate file and mapped it using templatefile function

--- code/v5_Functions_And_Looping/terraformCode/instances.tf
+++ code/v5_Functions_And_Looping/terraformCode/instances.tf
@@ -18,16 +18,8 @@ resource "aws_instance" "nginx" {
   iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
   depends_on             = [aws_iam_role_policy.allow_s3_all]
   tags                   = local.common_tags
-  user_data              = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/index.html /home/ec2-user/index.html
-aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
-sudo rm /usr/share/nginx/html/index.html
-sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
-sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png
-EOF
+  # Since we moved startup script to template and pass map argument that replaces s3_bucket_name with actual value
+  user_data              = templatefile("${path.module}/templates/startup_script.tpl", {s3_bucket_name = aws.aws_s3_bucket.web_bucket.id})
 }
 
 # # Creating AWS EC2 instance - Taking AMI from Data, using t2.micro as instance type, placing in subnet created earlier and assigning security groups
